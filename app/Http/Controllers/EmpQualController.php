979a0dd30d887e7b0e7587cfa26699260f336038
0re5ama <shahbibek10@gmail.com>
2018-06-20 22:02:56 +0545

    added 3 models

---
+++ app/Http/Controllers/EmpQualController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\EmpQual;
+use Illuminate\Http\Request;
+
+class EmpQualController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\EmpQual  $empQual
+     * @return \Illuminate\Http\Response
+     */
+    public function show(EmpQual $empQual)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\EmpQual  $empQual
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(EmpQual $empQual)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\EmpQual  $empQual
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, EmpQual $empQual)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\EmpQual  $empQual
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(EmpQual $empQual)
+    {
+        //
+    }
+}
